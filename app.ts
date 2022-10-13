ee435672a4533fda42f7c48305c43cbf042c1c19
leonardo <leoluchini2022@gmail.com>
2022-10-13 20:33:23 -0300

    animal

--- app.ts
+++ app.ts
@@ -1,1 +1,54 @@
-console.log('hola mundo  2 ')
+class Animal {
+    private tipo: string;
+    private edad: number;
+    protected tipoComida: string;
+
+
+    constructor(tipo: string, edad: number){
+        this.tipo=tipo;
+        this.edad= edad;
+        this.tipoComida = "";
+    }
+
+
+    public comer():void {
+        this.tipoComida = "";
+    }
+}
+
+
+class Felino extends Animal{
+    constructor(tipo: string, edad: number){
+        super(tipo,edad);
+    }
+
+    public comer():void {
+        this.tipoComida = 'carne';
+    }
+
+    public rugir() :void{
+        console.log('rugiendooo!');
+    }
+}
+
+
+class Pez extends Animal{
+    constructor(tipo: string, edad: number){
+        super(tipo,edad);
+    }
+    comer():void {
+        this.tipoComida = 'insectos';
+    }
+}
+
+const felino = new Felino('León', 2);
+
+felino.comer();
+console.log(felino);
+felino.rugir();
+const pez = new Pez("Pejerrey", 0.3);
+
+pez.comer();
+
+
+console.log(pez);
